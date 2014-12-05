73410ef1098d694b3623669827771f5367fbb184
Jack Le Hamster <jacklehamster@gmail.com>
2014-12-04 23:40:14 -0800

    Update list.js

--- list.js
+++ list.js
@@ -1,7 +1,13 @@
 (function list() {
   var demos = [
+    
+    //  START LIST
+    
     "?videoID=vbU2VXGjQj8&start=4:24&end=4:29.9",
     "?videoID=9OO4zxO9isk&start=30.75&end=42.46"
+    
+    //  END LIST
+    
   ];
   demoSearch = demos[Math.floor(Math.random()*demos.length)];
 })();
