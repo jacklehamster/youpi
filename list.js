7439d3c87f215bfeec0ab643b29677b86c93b8da
Jack Le Hamster <jacklehamster@gmail.com>
2014-12-04 23:35:33 -0800

    Create list.js

---
+++ list.js
@@ -0,0 +1,7 @@
+function() {
+  var demos = [
+    "?videoID=vbU2VXGjQj8&start=4:24&end=4:29.9",
+    "?videoID=9OO4zxO9isk&start=30.75&end=42.46"
+  ];
+  demoSearch = demos[Math.floor(Math.random()*demos.length)];
+}();
