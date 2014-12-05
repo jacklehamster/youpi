9f9517b3d706a506f13f08b3a8af1833e154009c
Jack Le Hamster <jacklehamster@gmail.com>
2014-12-04 23:40:25 -0800

    Update list.js

--- list.js
+++ list.js
@@ -4,7 +4,7 @@
     //  START LIST
     
     "?videoID=vbU2VXGjQj8&start=4:24&end=4:29.9",
-    "?videoID=9OO4zxO9isk&start=30.75&end=42.46"
+    "?videoID=9OO4zxO9isk&start=30.75&end=42.46",
     
     //  END LIST
     
