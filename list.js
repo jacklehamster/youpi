8eb8245eba7d9f21bfc451ad4db8ef278b70988b
Jack Le Hamster <jacklehamster@gmail.com>
2017-06-17 07:28:08 -0700

    Remove logs. Enable mouseOver in demo

--- list.js
+++ list.js
@@ -2,7 +2,7 @@
   var demos = [
     
     //  START LIST
-    "?videoID=2VzE8Ii12Js&start=64.7&end=78.3", //  youpi
+    "?videoID=2VzE8Ii12Js&start=64.7&end=78.2", //  youpi
     "?videoID=9OO4zxO9isk&start=30.75&end=42.46", //  van damn
     "?videoID=5yGJGTjV2WE&start=47.9&end=1:01.84", // no TV and no Beer
     "?videoID=OQSNhk5ICTI&start=30&end=62", //  double rainbow
